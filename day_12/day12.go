edf187e71df65e5e98b82bf3884790412435d6b5
Mike Digdon <32070772+digdon@users.noreply.github.com>
2023-12-14 12:45:33 -0400

    A complete re-write for day 12, including a solution for part 2

--- day_12/day12.go
+++ day_12/day12.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -23,220 +22,108 @@ func main() {
 		os.Exit(1)
 	}
 
-	var part1Sum int
-
+	var part1Sum int64
 	for _, line := range inputLines {
-		// value := calculateArrangementsNew(line)
-		value := calc(line, 1)
-		fmt.Println(line, "->", value)
-		part1Sum += value
+		sum := calc(line, 1)
+		// fmt.Println(line, "->", sum)
+		part1Sum += sum
 	}
-
 	fmt.Println("Part 1:", part1Sum)
 
-	// inputLine := "???.### 1,1,3"
-	// inputLine := ".??..??...?##. 1,1,3"
-	// inputLine := "?###???????? 3,2,1"
-	// inputLine := "??.?###????????? 2,4,4"
-	// fmt.Println(calc(inputLine, 1))
+	var part2Sum int64
+	for _, line := range inputLines {
+		sum := calc(line, 5)
+		// fmt.Println(line, "->", sum)
+		part2Sum += sum
+	}
+	fmt.Println("Part 2:", part2Sum)
 }
 
-func calc(input string, copies int) int {
+type CacheKey struct {
+	springPos, groupPos, contiguous int
+}
+
+func calc(input string, copies int) int64 {
 	splitPos := strings.Index(input, " ")
 	springDataString := input[:splitPos]
 	groupDataString := input[splitPos+1:]
 
 	if copies > 1 {
-		tempSprintData := make([]string, copies)
-		tempGroupData := make([]string, copies)
-
-		for i := 0; i < copies; i++ {
-			tempSprintData[i] = springDataString
-			tempGroupData[i] = groupDataString
-		}
-
-		springDataString = strings.Join(tempSprintData, "?")
-		groupDataString = strings.Join(tempGroupData, ",")
+		springDataString += strings.Repeat("?"+springDataString, copies-1)
+		groupDataString += strings.Repeat(","+groupDataString, copies-1)
 	}
 
-	// fmt.Println(springDataString)
-	// fmt.Println(groupDataString)
-	// return 0
-	springData := []byte(springDataString)
-
-	// Work out the spring groupings
-	groupSepRE := regexp.MustCompile(`,`)
-	matches := groupSepRE.Split(groupDataString, -1)
-	groupList := []int{}
-	for _, match := range matches {
-		value, _ := strconv.Atoi(match)
-		groupList = append(groupList, value)
+	springs := []byte(springDataString)
+	groupStrings := strings.Split(groupDataString, ",")
+	groups := make([]int, len(groupStrings))
+	for i, str := range groupStrings {
+		groups[i], _ = strconv.Atoi(str)
 	}
 
-	return walkArrangements(springData, groupList, 0, 0)
+	cache := map[CacheKey]int64{}
+	return walkArrangements(cache, springs, groups, 0, 0, 0)
 }
 
-func walkArrangements(springData []byte, groupList []int, pos int, contiguous int) int {
-	// fmt.Println(string(springData))
+func walkArrangements(cache map[CacheKey]int64, springs []byte, groups []int, springPos, groupPos, contiguous int) int64 {
+	cacheKey := CacheKey{springPos: springPos, groupPos: groupPos, contiguous: contiguous}
+	value, ok := cache[cacheKey]
 
-	for ; pos < len(springData); pos++ {
-		char := springData[pos]
-
-		if char == '?' {
-			tempSpringData := make([]byte, len(springData))
-			copy(tempSpringData, springData)
-			tempSpringData[pos] = '.'
-			count := walkArrangements(tempSpringData, groupList, pos, contiguous)
-			tempSpringData[pos] = '#'
-			count += walkArrangements(tempSpringData, groupList, pos, contiguous)
-			return count
-		} else if char == '#' {
-			if len(groupList) == 0 {
-				// fmt.Println("\tfailed because we ran out of groups")
-				return 0
-			}
-
-			contiguous++
-			if contiguous > groupList[0] {
-				// fmt.Printf("\tfailed because cont %d > %v\n", contiguous, groupList)
-				return 0
-			}
-		} else if char == '.' {
-			if contiguous > 0 {
-				if contiguous != groupList[0] {
-					// fmt.Printf("\tfailed because cont %d != %v\n", contiguous, groupList)
-					return 0
-				}
-
-				groupList = groupList[1:]
-				contiguous = 0
-			}
-		}
+	if ok {
+		// fmt.Printf("Cache hit for %v -> %d\n", cacheKey, value)
+		return value
 	}
 
-	if springData[pos-1] == '#' {
-		if len(groupList) != 1 {
-			// fmt.Println("\t failed because not all groups matched")
-			return 0
-		} else if contiguous == groupList[0] {
-			// fmt.Println("Success!", string(springData))
+	// fmt.Println(depth, springs, groups, springPos, groupPos, contiguous)
+	if springPos == len(springs) {
+		// We've reached the end
+		// Have all of the groups been matched with no leftover counts?
+		if groupPos == len(groups) && contiguous == 0 {
 			return 1
-		} else {
-			// fmt.Printf("\tfailed because cont %d > %v\n", contiguous, groupList)
-			return 0
 		}
-	} else {
-		if len(groupList) == 0 {
-			// fmt.Println("Success!", string(springData))
+
+		// If we have leftover counts, do we have a single group left and do they match?
+		if groupPos == len(groups)-1 && contiguous == groups[groupPos] {
 			return 1
+		}
+
+		// Unmatched groups and/or leftover counts mean this arrangement failed
+		return 0
+	}
+
+	var count int64
+
+	char := springs[springPos]
+
+	if char == '?' {
+		// First act as a '.'
+		if contiguous == 0 {
+			// Nothing contiguous, so continue processing
+			count += walkArrangements(cache, springs, groups, springPos+1, groupPos, contiguous)
 		} else {
-			// fmt.Println("\t failed because not all groups matched")
-			return 0
-		}
-	}
-}
-
-func calculateArrangementsNew(inputLine string) int {
-	var validCount int
-
-	pos := strings.Index(inputLine, " ")
-	springData := inputLine[:pos]
-	groupData := inputLine[pos+1:]
-
-	// Work out the spring groupings
-	groupSepRE := regexp.MustCompile(`,`)
-	matches := groupSepRE.Split(groupData, -1)
-	groupList := []int{}
-	for _, match := range matches {
-		value, _ := strconv.Atoi(match)
-		groupList = append(groupList, value)
-	}
-
-	// fmt.Println(springData)
-	// fmt.Println(groupList)
-
-	// Work out the number of unknown springs
-	unknownRE := regexp.MustCompile(`\?`)
-	unknownCount := len(unknownRE.FindAllStringIndex(springData, -1))
-
-	for arr := 0; arr < (1 << unknownCount); arr++ {
-		// fmt.Println("Arr:", arr)
-		elements := []byte{}
-		contiguous := 0
-		groupPos := 0
-		arrangementGood := true
-
-		for springNum, i := 0, 0; i < len(springData); i++ {
-			char := springData[i]
-
-			if char == '?' {
-				springValue := 1 << springNum
-
-				if springValue&arr == springValue {
-					char = '#'
-				} else {
-					char = '.'
-				}
-
-				springNum++
-			}
-
-			if char == '#' {
-				if groupPos >= len(groupList) {
-					arrangementGood = false
-					break
-				}
-
-				contiguous++
-
-				if contiguous > groupList[groupPos] {
-					arrangementGood = false
-				}
-			} else {
-				if i > 0 && elements[i-1] == '#' {
-					// End of a possible contiguous group - count to be sure
-					if contiguous != groupList[groupPos] {
-						arrangementGood = false
-					} else {
-						groupPos++
-					}
-				}
-
-				contiguous = 0
-			}
-
-			elements = append(elements, char)
-
-			if !arrangementGood {
-				break
+			// Do we have a group match?
+			if groupPos < len(groups) && contiguous == groups[groupPos] {
+				// A match - increment group and reset contiguous
+				count += walkArrangements(cache, springs, groups, springPos+1, groupPos+1, 0)
 			}
 		}
 
-		// fmt.Println(string(elements))
-
-		if len(elements) < len(springData) {
-			// Not a full string, so it must be invalid
-			continue
-		}
-
-		if elements[len(elements)-1] == '#' {
-			if contiguous != groupList[groupPos] {
-				continue
-			} else {
-				groupPos++
+		// Now act as a '#'
+		count += walkArrangements(cache, springs, groups, springPos+1, groupPos, contiguous+1)
+	} else if char == '.' {
+		if contiguous == 0 {
+			// Nothing contiguous, so continue processing
+			count += walkArrangements(cache, springs, groups, springPos+1, groupPos, contiguous)
+		} else {
+			// Do we have a group match?
+			if groupPos < len(groups) && contiguous == groups[groupPos] {
+				// A match - increment group and reset contiguous
+				count += walkArrangements(cache, springs, groups, springPos+1, groupPos+1, 0)
 			}
 		}
-
-		if groupPos < len(groupList) {
-			// Missed some of the groups, so it must be invalid
-			continue
-		}
-
-		if arrangementGood {
-			// fmt.Println("Good:", string(elements))
-			validCount++
-		}
+	} else { //if char == '#' {
+		count += walkArrangements(cache, springs, groups, springPos+1, groupPos, contiguous+1)
 	}
 
-	return validCount
+	cache[cacheKey] = count
+	return count
 }
