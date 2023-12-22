7524f25cbf0fa5c5b66e944bf38f550f9ca20680
Mike Digdon <32070772+digdon@users.noreply.github.com>
2023-12-22 11:03:46 -0400

    Fixed up an error in the calculation for part 2 that now returns the correct result.

--- day_21/README.md
+++ day_21/README.md
@@ -10,3 +10,5 @@ I tried following along with [this](https://github.com/villuna/aoc23/wiki/A-Geom
 Tired of wasting another single moment on this, I went to [this](https://github.com/mgtezak/Advent_of_Code/blob/master/2023/Day_21.py) to get some code to produce an answer. Thankfully it worked.
 
 Got the star, and I fully admit to requiring outright theft to do it. Never let us speak of this again.
+
+*UPDATE*: Ok, I couldn't let it sit. I went back through the math and realized my error. I need to add up all of the full even blocks, all of the full odd blocks, all of the even corners, and then substract all of the odd corners. My original math was: (all even blocks) + (all odd blocks) - (all odd corners + all even corners). ie, I was substracting all of the corners, instead of just the odd ones. Fixed it up and now my code gets the correct answer.
--- day_21/day21.go
+++ day_21/day21.go
@@ -35,6 +35,7 @@ func main() {
 	}
 
 	// part1(obstacles, maxRows, maxCols, startPoint)
+
 	visited := map[Point]int{}
 	queue := []data{{startPoint, 0}}
 
@@ -62,8 +63,8 @@ func main() {
 	}
 
 	part1Tiles := 0
-	var evenCornerTiles, oddCornerTiles uint64 = 0, 0
-	var evenBlockTiles, oddBlockTiles uint64 = 0, 0
+	evenCornerTiles, oddCornerTiles := 0, 0
+	evenBlockTiles, oddBlockTiles := 0, 0
 
 	for _, value := range visited {
 		if value%2 == 0 {
@@ -71,7 +72,7 @@ func main() {
 
 			if value <= 64 {
 				part1Tiles++
-			} else {
+			} else if value > 65 {
 				evenCornerTiles++
 			}
 		} else {
@@ -86,9 +87,9 @@ func main() {
 	fmt.Println("Part 1:", part1Tiles)
 
 	// Part 2 foolishness
-	var n uint64 = uint64((26501365 - maxCols/2) / maxCols)
-	var even, odd uint64 = n * n, (n + 1) * (n + 1)
-	var part2 uint64 = (odd * oddBlockTiles) + (even * evenBlockTiles) - (((n + 1) * oddCornerTiles) + (n * evenCornerTiles))
+	n := (26501365 - maxCols/2) / maxCols
+	even, odd := n*n, (n+1)*(n+1)
+	part2 := (odd * oddBlockTiles) + (even * evenBlockTiles) + (n * evenCornerTiles) - ((n + 1) * oddCornerTiles)
 	fmt.Println("Part 2:", part2)
 }
 
@@ -97,33 +98,33 @@ type data struct {
 	dist  int
 }
 
-func part1(obstacles map[Point]bool, maxRows, maxCols int, startPoint Point) {
-	tiles := map[Point]bool{startPoint: true}
+// func part1(obstacles map[Point]bool, maxRows, maxCols int, startPoint Point) {
+// 	tiles := map[Point]bool{startPoint: true}
 
-	for step := 0; step < 135; step++ {
-		nextTiles := map[Point]bool{}
+// 	for step := 0; step < 64; step++ {
+// 		nextTiles := map[Point]bool{}
 
-		for currPoint := range tiles {
+// 		for currPoint := range tiles {
 
-			for i := 0; i < len(DIRECTIONS); i++ {
-				nextRow, nextCol := currPoint.row+DIRECTIONS[i][0], currPoint.col+DIRECTIONS[i][1]
-				nextPoint := Point{nextRow, nextCol}
+// 			for i := 0; i < len(DIRECTIONS); i++ {
+// 				nextRow, nextCol := currPoint.row+DIRECTIONS[i][0], currPoint.col+DIRECTIONS[i][1]
+// 				nextPoint := Point{nextRow, nextCol}
 
-				if nextRow < 0 || nextRow >= maxRows || nextCol < 0 || nextCol >= maxCols {
-					continue
-				} else if !obstacles[nextPoint] {
-					nextTiles[nextPoint] = true
-				}
-			}
-		}
+// 				if nextRow < 0 || nextRow >= maxRows || nextCol < 0 || nextCol >= maxCols {
+// 					continue
+// 				} else if !obstacles[nextPoint] {
+// 					nextTiles[nextPoint] = true
+// 				}
+// 			}
+// 		}
 
-		tiles = nextTiles
+// 		tiles = nextTiles
 
-		fmt.Println(step+1, len(tiles))
-	}
+// 		fmt.Println(step+1, len(tiles))
+// 	}
 
-	fmt.Println("Part 1:", len(tiles))
-}
+// 	fmt.Println("Part 1:", len(tiles))
+// }
 
 var DIRECTIONS = [][]int{
 	{0, 1},  // R
